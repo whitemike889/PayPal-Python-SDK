a453746d418b4749ff00ee93d8d86b2396f4560c
Avi Das <adas@colgate.edu>
2014-03-13 09:47:15 -0500

    PEP8 compliance

--- paypalrestsdk/openid_connect.py
+++ paypalrestsdk/openid_connect.py
@@ -4,6 +4,7 @@ import paypalrestsdk.api as api
 from paypalrestsdk.version import __version__
 from six import string_types
 
+
 class Base(Resource):
 
     user_agent = "PayPalSDK/openid-connect-python %s (%s)" % (__version__, api.Api.library_details)
@@ -85,6 +86,7 @@ class Userinfo(Base):
 def endpoint():
     return api.default().options.get("openid_endpoint", api.default().endpoint)
 
+
 def client_id():
     return api.default().options.get("openid_client_id", api.default().client_id)
 
@@ -100,12 +102,14 @@ def redirect_uri():
 start_session_path = "/webapps/auth/protocol/openidconnect/v1/authorize"
 end_session_path = "/webapps/auth/protocol/openidconnect/v1/endsession"
 
+
 def session_url(path, options=None):
     if api.default().mode == "live":
         path = util.join_url("https://www.paypal.com", path)
     else:
         path = util.join_url("https://www.sandbox.paypal.com", path)
     return util.join_url_params(path, options or {})
+
 
 def authorize_url(options=None):
     options = util.merge_dict({

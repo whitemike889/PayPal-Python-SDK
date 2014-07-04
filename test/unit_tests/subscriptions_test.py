60016574238459c2b8838632dc13e8f1930fc90c
Avi Das <adas@colgate.edu>
2014-07-04 12:33:58 -0500

    Correct tests for billing plans and agreements

--- test/unit_tests/subscriptions_test.py
+++ test/unit_tests/subscriptions_test.py
@@ -7,50 +7,77 @@ class TestBillingPlan(unittest.TestCase):
 
     def setUp(self):
         self.billing_plan_attributes = {
-            "name": "Fast Speed Plan",
-            "description": "Template creation.",
-            "type": "fixed",
+            "description": "Create Plan for Regular",
+            "merchant_preferences": {
+                "auto_bill_amount": "yes",
+                "cancel_url": "http://www.cancel.com",
+                "initial_fail_amount_action": "continue",
+                "max_fail_attempts": "1",
+                "return_url": "http://www.success.com",
+                "setup_fee": {
+                    "currency": "USD",
+                    "value": "25"
+                }
+            },
+            "name": "Testing1-Regular1",
             "payment_definitions": [
                 {
-                    "name": "Payment Definition-1",
-                    "type": "REGULAR",
-                    "frequency": "MONTH",
-                    "frequency_interval": "2",
                     "amount": {
-                        "value": "100",
-                        "currency": "USD"
+                        "currency": "USD",
+                        "value": "100"
                     },
-                    "cycles": "12",
                     "charge_models": [
                         {
-                            "type": "SHIPPING",
                             "amount": {
-                                "value": "10",
-                                "currency": "USD"
-                            }
+                                "currency": "USD",
+                                "value": "10.60"
+                            },
+                            "type": "SHIPPING"
                         },
                         {
-                            "type": "TAX",
                             "amount": {
-                                "value": "12",
-                                "currency": "USD"
-                            }
+                                "currency": "USD",
+                                "value": "20"
+                            },
+                            "type": "TAX"
                         }
-                    ]
+                    ],
+                    "cycles": "0",
+                    "frequency": "MONTH",
+                    "frequency_interval": "1",
+                    "name": "Regular 1",
+                    "type": "REGULAR"
+                },
+                {
+                    "amount": {
+                        "currency": "USD",
+                        "value": "20"
+                    },
+                    "charge_models": [
+                        {
+                            "amount": {
+                                "currency": "USD",
+                                "value": "10.60"
+                            },
+                            "type": "SHIPPING"
+                        },
+                        {
+                            "amount": {
+                                "currency": "USD",
+                                "value": "20"
+                            },
+                            "type": "TAX"
+                        }
+                    ],
+                    "cycles": "4",
+                    "frequency": "MONTH",
+                    "frequency_interval": "1",
+                    "name": "Trial 1",
+                    "type": "TRIAL"
                 }
             ],
-            "merchant_preferences": {
-                "setup_fee": {
-                    "value": "1",
-                    "currency": "USD"
-                },
-                "return_url": "http://www.paypal.com",
-                "cancel_url": "http://www.yahoo.com",
-                "autobill_amount": "YES",
-                "initial_amount_fail_action": "CONTINUE",
-                "max_fail_attempts": "0"
-            }
-        }
+            "type": "INFINITE"
+        }        
         self.billing_plan_update_attributes = [
             {
                 "op": "replace",
@@ -61,39 +88,40 @@ class TestBillingPlan(unittest.TestCase):
             }
         ]
         self.billing_plan = paypal.BillingPlan(self.billing_plan_attributes)
-        self.billing_plan.id = 'P-0NJ10521L3680291SOAQIVTQ'
+        self.billing_plan_id = 'P-0NJ10521L3680291SOAQIVTQ'
 
     @patch('test_helper.paypal.Api.post', autospec=True)
     def test_create(self, mock):
         response = self.billing_plan.create()
 
-        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing_plans',
-            self.billing_plan_attributes, {'PayPal-Request-Id' : billing_plan.request_id}, None)
+        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing-plans',
+            self.billing_plan_attributes, {'PayPal-Request-Id' : ANY}, None)
         self.assertEqual(response, True)
 
     @patch('test_helper.paypal.Api.get', autospec=True)
     def test_find(self, mock):
-        billing_plan = paypal.BillingPlan.find(self.billing_plan.id)
+        billing_plan = paypal.BillingPlan.find(self.billing_plan_id)
 
-        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing_plans/' + self.billing_plan.id)
+        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing-plans/' + self.billing_plan_id)
         self.assertTrue(isinstance(billing_plan, paypal.BillingPlan))
-
+    
     @patch('test_helper.paypal.Api.get', autospec=True)
     def test_all(self, mock):
         mock.return_value = {'plans': [self.billing_plan_attributes]}
         history = paypal.BillingPlan.all({'status': 'CREATED'})
 
-        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing_plans?status=CREATED')
-        self.assertEqual(len(self.history.plans), 1)
-        self.assertTrue(isinstance(history.plans[0], paypal.BillingPlan))
-
+        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing-plans?status=CREATED')
+        self.assertEqual(len(history.plans), 1)
+    
     @patch('test_helper.paypal.Api.patch', autospec=True)
     def test_replace(self, mock):
+        self.billing_plan.id = self.billing_plan_id
         response = self.billing_plan.replace(self.billing_plan_update_attributes)
 
-        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing_plans/' + self.billing_plan.id \
-            , self.billing_plan_update_attributes, {'PayPal-Request-Id' : self.billing_plan.request_id}, None))
+        mock.assert_called_once_with(self.billing_plan.api, 'v1/payments/billing-plans/' + self.billing_plan_id \
+            , self.billing_plan_update_attributes, {'PayPal-Request-Id' : ANY}, None)
         self.assertEqual(response, True)
+
 
 class TestBillingAgreement(unittest.TestCase):
 
@@ -184,110 +212,34 @@ class TestBillingAgreement(unittest.TestCase):
         }
 
         self.billing_agreement = paypal.BillingAgreement(self.billing_agreement_attributes)
-        self.billing_agreement.id = "I-THNVHK6X9H0V"
+        self.billing_agreement_id = "I-THNVHK6X9H0V"
 
+    
     @patch('test_helper.paypal.Api.post', autospec=True)
     def test_create(self, mock):
         response = self.billing_agreement.create()
 
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements',
-            self.billing_agreement_attributes, {'PayPal-Request-Id' : billing_agreement.request_id}, None)
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements',
+            self.billing_agreement_attributes, {'PayPal-Request-Id' : ANY}, None)
         self.assertEqual(response, True)
-
+    
+    
     @patch('test_helper.paypal.Api.get', autospec=True)
     def test_find(self, mock):
 
-        billing_agreement = paypal.BillingAgreement.find(self.billing_agreement.id)
+        billing_agreement = paypal.BillingAgreement.find(self.billing_agreement_id)
 
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id)
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id)
         self.assertTrue(isinstance(billing_agreement, paypal.BillingAgreement))
 
     @patch('test_helper.paypal.Api.patch', autospec=True)
     def test_replace(self, mock):
+        self.billing_agreement.id = self.billing_agreement_id
         response = self.billing_agreement.replace(self.billing_agreement_update_attributes)
 
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id \
-            , self.billing_agreement_update_attributes, {'PayPal-Request-Id' : self.billing_agreement.request_id}, None))
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id \
+            , self.billing_agreement_update_attributes, {'PayPal-Request-Id' : ANY}, None)
         self.assertEqual(response, True)
-
-    @patch('test_helper.paypal.Api.post', autospec=True)
-    def test_suspend(self, mock):
-        suspend_attributes = {
-            "note": "Suspending the agreement"
-        }
-        response = self.billing_agreement.suspend(suspend_attributes)
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id + '/suspend', \
-         suspend_attributes, {'PayPal-Request-Id' : ANY})
-        self.assertEqual(response, True)
-
-    @patch('test_helper.paypal.Api.post', autospec=True)
-    def test_reactivate(self, mock):
-        reactivate_attributes = {
-            "note": "Reactivating the agreement"
-        }
-        response = self.billing_agreement.reactivate(reactivate_attributes)
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id + '/re-activate', \
-         reactivate_attributes, {'PayPal-Request-Id' : ANY})
-        self.assertEqual(response, True)
-
-    @patch('test_helper.paypal.Api.post', autospec=True)
-    def test_cancel(self, mock):
-        cancel_attributes = {
-            "note": "Canceling the agreement"
-        }
-        response = self.billing_agreement.cancel(cancel_attributes)
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id + '/cancel', \
-         cancel_attributes, {'PayPal-Request-Id' : ANY})
-        self.assertEqual(response, True)
-
-    @patch('test_helper.paypal.Api.post', autospec=True)
-    def test_set_balance(self, mock):
-        set_balance_attributes = {
-            "value" : "10",
-            "currency" : "USD"
-        }
-        response = self.billing_agreement.set_balance(set_balance_attributes)
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id + '/set-balance', \
-         set_balance_attributes, {'PayPal-Request-Id' : ANY})
-        self.assertEqual(response, True)
-
-    @patch('test_helper.paypal.Api.post', autospec=True)
-    def test_bill_balance(self, mock):
-        bill_balance_attributes = {
-            "note": "Billing Balance Amount",
-            "amount": {
-                "value": "10",
-                "currency": "USD"
-            }
-        }
-        response = self.billing_agreement.bill_balance(bill_balance_attributes)
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' + self.billing_agreement.id + '/bill-balance', \
-         bill_balance_attributes, {'PayPal-Request-Id' : ANY})
-        self.assertEqual(response, True)
-
-    @patch('test_helper.paypal.Api.post', autospec=True)
-    def test_execute(self, mock):
-        response = self.billing_agreement.create()
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements',
-            self.billing_agreement_attributes, {'PayPal-Request-Id' : ANY}, None)
-        self.assertEqual(response, True)
-
-        self.billing_agreement = self.billing_agreement_attributes_created
-        mock.return_value = self.billing_agreement_executed_attributes
-
-        response = billing_agreement.execute()
-        # Test that execute actually makes a http post to the href element of links array with id
-        execute_url = [link['href'] for link in self.billing_agreement_attributes_created.links if link['rel'] == 'execute'][0]
-        execute_url_path = urlparse.urlparse(execute_url).path
-
-        mock.assert_called_with(self.billing_agreement.api, execute_url_path, {}, {'PayPal-Request-Id' : ANY})
-        self.assertTrue(isinstance(response, paypal.Resource))
 
     @patch('test_helper.paypal.Api.get', autospec=True)
     def test_search_transactions(self, mock):
@@ -325,8 +277,90 @@ class TestBillingAgreement(unittest.TestCase):
                 }
             ]
         }
+        self.billing_agreement.id = self.billing_agreement_id
+        print self.billing_agreement
         transactions = self.billing_agreement.search_transactions(date_range)
-
-        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing_agreements/' \
-        + self.billing_agreement.id + '/transaction?start-date=2014-07-20&end-date=2014-07-30', {'PayPal-Request-Id' : ANY})
         self.assertEqual(len(transactions.agreement_transaction_list), 2)
+
+    @patch('test_helper.paypal.Api.post', autospec=True)
+    def test_suspend(self, mock):
+        suspend_attributes = {
+            "note": "Suspending the agreement"
+        }
+        self.billing_agreement.id = self.billing_agreement_id
+        response = self.billing_agreement.suspend(suspend_attributes)
+
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id + '/suspend', \
+         suspend_attributes, {'PayPal-Request-Id' : ANY})
+        self.assertEqual(response, True)
+
+    @patch('test_helper.paypal.Api.post', autospec=True)
+    def test_reactivate(self, mock):
+        reactivate_attributes = {
+            "note": "Reactivating the agreement"
+        }
+        self.billing_agreement.id = self.billing_agreement_id
+        response = self.billing_agreement.reactivate(reactivate_attributes)
+
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id + '/re-activate', \
+         reactivate_attributes, {'PayPal-Request-Id' : ANY})
+        self.assertEqual(response, True)
+
+    @patch('test_helper.paypal.Api.post', autospec=True)
+    def test_cancel(self, mock):
+        cancel_attributes = {
+            "note": "Canceling the agreement"
+        }
+        self.billing_agreement.id = self.billing_agreement_id
+        response = self.billing_agreement.cancel(cancel_attributes)
+
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id + '/cancel', \
+         cancel_attributes, {'PayPal-Request-Id' : ANY})
+        self.assertEqual(response, True)
+
+    @patch('test_helper.paypal.Api.post', autospec=True)
+    def test_set_balance(self, mock):
+        set_balance_attributes = {
+            "value" : "10",
+            "currency" : "USD"
+        }
+        self.billing_agreement.id = self.billing_agreement_id
+        response = self.billing_agreement.set_balance(set_balance_attributes)
+
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id + '/set-balance', \
+         set_balance_attributes, {'PayPal-Request-Id' : ANY})
+        self.assertEqual(response, True)
+
+    @patch('test_helper.paypal.Api.post', autospec=True)
+    def test_bill_balance(self, mock):
+        bill_balance_attributes = {
+            "note": "Billing Balance Amount",
+            "amount": {
+                "value": "10",
+                "currency": "USD"
+            }
+        }
+        self.billing_agreement.id = self.billing_agreement_id
+        response = self.billing_agreement.bill_balance(bill_balance_attributes)
+
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements/' + self.billing_agreement_id + '/bill-balance', \
+         bill_balance_attributes, {'PayPal-Request-Id' : ANY})
+        self.assertEqual(response, True)
+
+    @patch('test_helper.paypal.Api.post', autospec=True)
+    def test_execute(self, mock):
+        response = self.billing_agreement.create()
+
+        mock.assert_called_once_with(self.billing_agreement.api, 'v1/payments/billing-agreements',
+            self.billing_agreement_attributes, {'PayPal-Request-Id' : ANY}, None)
+        self.assertEqual(response, True)
+
+        self.billing_agreement = paypal.BillingAgreement(self.billing_agreement_attributes_created)
+        mock.return_value = self.billing_agreement_executed_attributes
+
+        response = self.billing_agreement.execute()
+        # Test that execute actually makes a http post to the href element of links array with id
+        execute_url = [link['href'] for link in self.billing_agreement_attributes_created.get('links') if link['rel'] == 'execute'][0]
+        execute_url_path = urlparse.urlparse(execute_url).path[1:]
+
+        mock.assert_called_with(self.billing_agreement.api, execute_url_path, {}, {'PayPal-Request-Id' : ANY})

12ef6f1403b22cdd99bda696c829269722fb7c84
Avi Das <adas@colgate.edu>
2014-07-31 14:24:32 -0500

    Document app engine str unicode issue

--- samples/payment/create_with_paypal.py
+++ samples/payment/create_with_paypal.py
@@ -52,6 +52,8 @@ if payment.create():
   # Redirect the user to given approval url
   for link in payment.links:
     if link.method == "REDIRECT":
+      # Convert to str to avoid google appengine unicode issue
+      # https://github.com/paypal/rest-api-sdk-python/pull/58
       redirect_url = str(link.href)
       print("Redirect for approval: %s"%(redirect_url))
 else:

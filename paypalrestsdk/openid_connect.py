633cd1ec8544d510f33412cbf0c11e61d8fd6caa
Bryant Luk <bryant.luk@getbraintree.com>
2016-08-18 13:50:23 -0500

    Revert end_session_path change until endpoint is changed

--- paypalrestsdk/openid_connect.py
+++ paypalrestsdk/openid_connect.py
@@ -105,7 +105,7 @@ def redirect_uri():
 
 
 start_session_path = "/signin/authorize"
-end_session_path = "/signin/endsession"
+end_session_path = "/webapps/auth/protocol/openidconnect/v1/endsession"
 
 
 def session_url(path, options=None):

b2c8ebb9df29b7ec1844fc2d2c71c45fe99d1f31
Helgi Hrafn Gunnarsson <helgihrafn@1984.is>
2017-05-23 15:35:07 +0000

    Samples: Redirection for approval of payments no longer determined by HTTP method (link.method == "REDIRECT") but rather as specified by its intended purpose (link.rel == "approval_url").

--- samples/order/create.py
+++ samples/order/create.py
@@ -37,9 +37,9 @@ if payment.create():
     print("Payment[%s] created successfully" % (payment.id))
     # Redirect the user to given approval url
     for link in payment.links:
-        if link.method == "REDIRECT":
-            redirect_url = str(link.href)
-            print("Redirect for approval: %s" % (redirect_url))
+        if link.rel == "approval_url":
+            approval_url = str(link.href)
+            print("Redirect for approval: %s" % (approval_url))
 else:
     print("Error while creating payment:")
     print(payment.error)
